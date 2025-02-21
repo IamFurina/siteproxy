bbfb0418abe1f261756c19f9d0ecffe5310566d7
IamFurina <emotionui@qq.com>
2025-02-21 23:38:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.sqeven.vercel.app',
+    serverName: 'proxy.focalors.ltd',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
